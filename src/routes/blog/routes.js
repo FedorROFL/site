ca579c94181b57c0c0ebc4f7aeb169b858641a88
Fedor <vanteevfedor2008@gmail.com>
2023-05-24 19:00:43 +0800

    init

---
+++ src/routes/blog/routes.js
@@ -0,0 +1,14 @@
+export const routes = [
+    {
+      id: 1,
+      title: 'Почему работа программистом востребована в наши дни.',
+      body: ' В этой статье я расскажу почему работа программистом стала очень востребована в наши дни.',
+    },
+    {
+      "id": 2,
+      "title": "My Second Blog Post",
+      "body": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis dignissimos dolorem ea ex maiores minima minus nisi perspiciatis sapiente voluptate"
+    }
+    
+  ];
+  
